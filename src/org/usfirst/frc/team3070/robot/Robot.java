0781046599cb0168b72ddfd0f9fdb74f4032ec43
Team Pronto Laptop 1 <hughgaevert@gmail.com>
2015-03-19 15:54:07 -0700

    codeses

--- src/org/usfirst/frc/team3070/robot/Robot.java
+++ src/org/usfirst/frc/team3070/robot/Robot.java
@@ -35,28 +35,29 @@ public class Robot extends IterativeRobot implements Pronstants {
 
 	CameraServer camera;
 	// creating camera
-	
+
 	Timer timer;
-	
+
 	int autoState = 0;
 
-	double x, y, z; // it appears that x indicates forward/backward; y indicates left/right; z indicates turn left/right (JDS)
+	double x, y, z; // it appears that x indicates forward/backward; y indicates
+					// left/right; z indicates turn left/right (JDS)
 
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
 	public void robotInit() {
-		mFrontLeft  = new CANTalon(M_FRONT_LEFT_ID);  // front left wheel motor
+		mFrontLeft = new CANTalon(M_FRONT_LEFT_ID); // front left wheel motor
 		mFrontRight = new CANTalon(M_FRONT_RIGHT_ID); // front right wheel motor
-		mRearLeft   = new CANTalon(M_REAR_LEFT_ID);   // rear left wheel motor
-		mRearRight  = new CANTalon(M_REAR_RIGHT_ID);  // rear right wheel motor
-		mLift1 = new CANTalon(M_LIFT1_ID);            // two motors drive the lifter,
-		mLift2 = new CANTalon(M_LIFT2_ID);            // ... and here is the second motor
+		mRearLeft = new CANTalon(M_REAR_LEFT_ID); // rear left wheel motor
+		mRearRight = new CANTalon(M_REAR_RIGHT_ID); // rear right wheel motor
+		mLift1 = new CANTalon(M_LIFT1_ID); // two motors drive the lifter,
+		mLift2 = new CANTalon(M_LIFT2_ID); // ... and here is the second motor
 		mLoader = new CANTalon(M_LOADER_ID);
 		mFlexer = new CANTalon(M_FLEXER_ID);
 		// setting the CANTAlons
-		
+
 		mFrontLeft.setVoltageRampRate(RAMP_RATE);
 		mFrontRight.setVoltageRampRate(RAMP_RATE);
 		mRearLeft.setVoltageRampRate(RAMP_RATE);
@@ -66,7 +67,7 @@ public class Robot extends IterativeRobot implements Pronstants {
 		jLeft = new Joystick(LEFT_JOYSTICK_PORT);
 		jRight = new Joystick(RIGHT_JOYSTICK_PORT);
 		// setting joysticks
-		
+
 		upperLimit = new DigitalInput(UPPER_LIMIT_ID);
 		lowerLimit = new DigitalInput(LOWER_LIMIT_ID);
 		toteLimit = new DigitalInput(TOTE_LIMIT_ID);
@@ -79,7 +80,7 @@ public class Robot extends IterativeRobot implements Pronstants {
 		loader = new ProntoLoader(mLoader, jLeft, jRight);
 		flexer = new ProntoFlexer(mFlexer, jLeft);
 		// setting drive, load, lift, and flex classes
-		
+
 		timer = new Timer();
 
 		x = 0.0;
@@ -88,67 +89,49 @@ public class Robot extends IterativeRobot implements Pronstants {
 	}
 
 	public void autonomousInit() {
-//		if (camera == null)
-//			cameraSetup();
-//		// starts camera recording
-		
-//		mechDrive.setControlModePosition();
-//		// set drive to move based on position input
-		
-//		mechDrive.resetPosition();
-//		// zero the encoders
-		
+		// if (camera == null)
+		// cameraSetup();
+		// // starts camera recording
+
+		// mechDrive.setControlModePosition();
+		// // set drive to move based on position input
+		// mechDrive.resetPosition();
+		// // zero the encoders
+
 		timer.reset();
 		timer.start();
-		
-		}
+
+	}
 
 	/**
 	 * This function is called periodically during autonomous
 	 */
 	public void autonomousPeriodic() {
-		// Example: mechDrive.setPos(0, 2000, 0);
-		// goes 2000 encoder units straight
+		// Example: mechDrive.drive(0, 1, 0);
+		// Makes the robot drive at a velocity of one
 
-		// 1000 is about 16 inches
-		
-		if (timer.get() != 1.5) {
-			mechDrive.drive(0, 1.75, 0);
+		if (timer.get() < 1) {
+			mechDrive.drive(0, .5, 0);
+		} else if (timer.get() >= 1 && timer.get() >= 1.5) {
+			mLift1.set(-LIFT_SPEED);
+			mLift2.set(-LIFT_SPEED);
+		} else if (timer.get() > 1.5 && timer.get() < 2.5) {
+			mechDrive.drive(0, .5, 0);
+			mLift1.set(0.0);
+			mLift2.set(0.0);
 		} else {
-			mechDrive.drive(0,0,0);
+			mechDrive.drive(0, 0, 0);
 			timer.stop();
+			mLift1.set(0.0);
+			mLift2.set(0.0);
 		}
-		
-//		switch (autoState) {
-//		case 1:
-//			readyLifter();
-//			break;
-//		case 2:
-//			moveForward(1);
-//			break;
-//		case 3:
-//			liftUp();
-//			break;
-//		case 4:
-//			moveBackward(5);
-//			break;
-//		case 5:
-//			liftDown();
-//			break;
-//		case 6:
-//			moveBackward(1);
-//			break;
-//		default:
-//			break;
-//		}
-//		
-//		System.out.println("State: " + autoState);
+
 	}
 
 	public void teleopInit() {
 		mechDrive.setControlModeSpeed();
 		// set for velocity input
-		
+
 		mechDrive.setAllPID(KP, KI, KD);
 		// set P to driving value
 	}
@@ -199,14 +182,14 @@ public class Robot extends IterativeRobot implements Pronstants {
 	private void printToSmartDashboard() {
 		SmartDashboard.putBoolean(" At Top ", !ProntoLift.notAtTop);
 		// displays if cars are at absolute upper limit
-		
+
 		SmartDashboard.putBoolean(" At Bottom ", !ProntoLift.notAtBottom);
 		// displays if cars are at absolute lower limit
-		
+
 		SmartDashboard.putBoolean(" Ready For Tote ",
 				ProntoLift.readyForNextTote);
 		// displays if cars are in position for a tote
-		
+
 		SmartDashboard.putBoolean(" Flexer Contracted ", ProntoFlexer.flexedIn);
 		// displays if the arms are contracted
 	}
@@ -218,97 +201,97 @@ public class Robot extends IterativeRobot implements Pronstants {
 		camera.startAutomaticCapture();
 		// start recording
 	}
-	
+
 	private void readyLifter() {
 		// set cars fully up
 		while (upperLimit.get()) {
 			mLift1.set(-LIFT_SPEED);
 			mLift2.set(-LIFT_SPEED);
 		}
-		
+
 		// stop cars
 		mLift1.set(0);
 		mLift2.set(0);
-		
+
 		// position a car to tote level
 		while (toteLimit.get()) {
 			mLift1.set(LIFT_SPEED);
 			mLift2.set(LIFT_SPEED);
 		}
-		
+
 		// stop the lift
 		mLift1.set(0);
 		mLift2.set(0);
-		
+
 		// move down past the tote limit
 		while (!toteLimit.get()) {
 			mLift1.set(-0.4);
 			mLift2.set(-0.4);
 		}
-		
+
 		mLift1.set(0);
 		mLift2.set(0);
-		
+
 		autoState++;
 	}
-	
+
 	private void moveForward(int distance) {
 		// moves forward in increments of 16 inches
 		for (int i = 0; i < distance; i++) {
 			while (mechDrive.rearLeft.getPosition() != -1000) {
-				mechDrive.setPos(0,-1000,0);
+				mechDrive.setPos(0, -1000, 0);
 				// forward is negative distance
 			}
 			mechDrive.resetPosition();
 		}
-		
+
 		autoState++;
 	}
-	
+
 	private void moveBackward(int distance) {
 		// moves backward in increments of 16 inches
-				for (int i = 0; i < distance; i++) {
-					while (mechDrive.rearLeft.getPosition() != 1000) {
-						mechDrive.setPos(0,1000,0);
-						// backward is positive distance
-					}
-					mechDrive.resetPosition();
-				}
-				
-				autoState++;
+		for (int i = 0; i < distance; i++) {
+			while (mechDrive.rearLeft.getPosition() != 1000) {
+				mechDrive.setPos(0, 1000, 0);
+				// backward is positive distance
+			}
+			mechDrive.resetPosition();
+		}
+
+		autoState++;
 	}
-	
+
 	private void liftUp() {
 		while (upperLimit.get()) {
 			// while not fully up, lifting up
 			mLift1.set(-LIFT_SPEED);
 			mLift2.set(-LIFT_SPEED);
 		}
-		
+
 		mLift1.set(0);
 		mLift2.set(0);
-		
+
 		autoState++;
 	}
-	
+
 	private void liftDown() {
 		while (toteLimit.get()) {
 			// while not at the tote level, lifting down
 			mLift1.set(LIFT_SPEED);
 			mLift2.set(LIFT_SPEED);
 		}
-		
+
 		while (!lowerLimit.get()) {
 			// move past the tote limit
 			mLift1.set(LIFT_SPEED);
 			mLift2.set(LIFT_SPEED);
 		}
-		
+
 		mLift1.set(0);
 		mLift2.set(0);
-		
+
 		autoState++;
-		
+
 		// tote should be unhooked
 	}
 }
